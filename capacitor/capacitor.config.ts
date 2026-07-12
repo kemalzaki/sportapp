8bfb3dd77a23ef36df40cd482dbf940fb551bd4c
Syndrom2211 <yukmari2211@gmail.com>
2026-07-12 12:34:33 +0700

    Rev

--- capacitor/capacitor.config.ts
+++ capacitor/capacitor.config.ts
@@ -1,21 +1,14 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
+  appId: 'com.yukmari.kawankeringatappsport',
+  appName: 'KawanKeringat AppSport',
+  webDir: 'www',
 
-appId: 'com.yukmari.kawankeringat',
-
-appName: 'KawanKeringat',
-
-webDir: 'www',
-
-server: {
-
-url: 'https://hapfam.alwaysdata.net/',
-
-cleartext: true
-
-}
-
+  server: {
+    url: 'https://hapfam.alwaysdata.net/',
+    cleartext: true
+  }
 };
 
 export default config;
